46d1a398d06e48fc54f2d301c44ce552855a8e1f
justin <978336184@qq.com>
2022-03-15 12:28:49 +0800

    架构初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <router-view v-if="isInit"></router-view>
+    <Host v-else/>
+</template>
+
+<script setup lang="ts">
+import { userPlayerInit } from "@/stores/player";
+import { toRefs } from "vue";
+import { useHostStore } from "@/stores/host";
+import Host from "@/Host.vue";
+
+userPlayerInit();
+
+const { isInit } = toRefs(useHostStore());
+</script>
